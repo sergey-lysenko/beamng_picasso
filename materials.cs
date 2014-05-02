05e7070046b12a39de085fac1b1d8be4c85c9a91
Sergey Lysenko <sergey@lysenko.kiev.ua>
2014-05-02 21:09:48 +0300

    Fixed merge issue

---
+++ materials.cs
@@ -0,0 +1,249 @@
+
+singleton Material(picasso_Suspension)
+{
+   mapTo = "Suspension";
+   diffuseColor[0] = "0.0317435 0.0317435 0.0317435 1";
+   specular[0] = "0.249724 0.250924 0.254523 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Black_plastic)
+{
+   mapTo = "Black_plastic";
+   diffuseColor[0] = "0.0173863 0.0173863 0.0173863 1";
+   specular[0] = "0.0908344 0.0908344 0.0908344 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Green_Glass)
+{
+   mapTo = "Green_Glass";
+   diffuseColor[0] = "0.8 0.996078 0.784314 0.2";
+   specular[0] = "0.996078 0.996078 0.996078 1";
+   specularPower[0] = "128";
+   doubleSided = "0";
+   translucent = "1";
+   pixelSpecular[0] = "1";
+   castShadows = "0";
+   alphaTest = "1";
+   alphaRef = "0";
+   cubemap = "BlueSkyCubemap";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(picasso_Body)
+{
+   mapTo = "Body";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.249724 0.250924 0.254523 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Black_rubber)
+{
+   mapTo = "Black_rubber";
+   diffuseColor[0] = "0.0203776 0.0203776 0.0203776 1";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Red_Glass)
+{
+   mapTo = "Red_Glass";
+   diffuseColor[0] = "0.2 0 0 0.9";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucent = "1";
+};
+
+singleton Material(picasso_Chrome_001)
+{
+   mapTo = "Chrome_001";
+   diffuseColor[0] = "0.6 0.6 0.6 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Carpet)
+{
+   mapTo = "Carpet";
+   diffuseColor[0] = "0.06 0.06 0.06 1";
+   specular[0] = "0.110689 0.110689 0.110689 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_White_Rubber)
+{
+   mapTo = "White_Rubber";
+   diffuseColor[0] = "0.615177 0.615177 0.615177 1";
+   specular[0] = "0.507068 0.507068 0.507068 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Turnlight_glass)
+{
+   mapTo = "Turnlight_glass";
+   diffuseColor[0] = "1 0.5 0 0.727941";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucent = "1";
+};
+
+singleton Material(picasso_Gray_plastic)
+{
+   mapTo = "Gray_plastic";
+   diffuseColor[0] = "0.12 0.12 0.12 1";
+   specular[0] = "0.0908344 0.0908344 0.0908344 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_White_cloth)
+{
+   mapTo = "White_cloth";
+   diffuseColor[0] = "0.64 0.633526 0.497977 1";
+   specular[0] = "0.249724 0.250924 0.254523 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Body_001)
+{
+   mapTo = "Body_001";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.249724 0.250924 0.254523 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Black_rubber_001)
+{
+   mapTo = "Black_rubber_001";
+   diffuseColor[0] = "0.0163021 0.0163021 0.0163021 1";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Body_000)
+{
+   mapTo = "Body_000";
+   diffuseColor[0] = "0.159372 0.203656 0.558557 1";
+   specular[0] = "0.249724 0.250924 0.254523 1";
+   specularPower[0] = "75";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Aluminum)
+{
+   mapTo = "Aluminum";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Amber_light)
+{
+   mapTo = "Amber_light";
+   diffuseColor[0] = "0.64 0.514515 0.115009 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_White_light)
+{
+   mapTo = "White_light";
+   diffuseColor[0] = "0.8 0.8 0.8 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_White_Glass)
+{
+   mapTo = "White_Glass";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Body_Polished)
+{
+   mapTo = "Body_Polished";
+   diffuseColor[0] = "0.159372 0.203656 0.558557 1";
+   specular[0] = "0.249724 0.250924 0.254523 1";
+   specularPower[0] = "75";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_White_cloth_001)
+{
+   mapTo = "White_cloth_001";
+   diffuseColor[0] = "0.64 0.633526 0.497977 1";
+   specular[0] = "0.254523 0.253492 0.232103 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_White_plastic)
+{
+   mapTo = "White_plastic";
+   diffuseColor[0] = "0.813669 0.814041 0.726535 1";
+   specular[0] = "0.581463 0.581463 0.581463 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(picasso_Black_Glass)
+{
+   mapTo = "Black_Glass";
+   diffuseColor[0] = "0.192093 0.2 0.192207 0.9";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucent = "1";
+};
+
+singleton Material(picasso_Clear_Glass)
+{
+   mapTo = "Clear_Glass";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 0.2";
+   specular[0] = "0.996078 0.996078 0.996078 1";
+   specularPower[0] = "11";
+   doubleSided = "1";
+   translucent = "1";
+   pixelSpecular[0] = "1";
+   castShadows = "0";
+   materialTag0 = "beamng";
+   materialTag2 = "Industrial";
+   materialTag1 = "beamng";
+};
